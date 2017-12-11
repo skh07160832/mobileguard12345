7438de238d1042c3f6bc1d39086908e89d99a1c7
施凯焕 <31606844+skh07160832@users.noreply.github.com>
2017-12-11 14:29:10 +0800

    Update init.inc.php

--- common/smarty/init.inc.php
+++ common/smarty/init.inc.php
@@ -1,18 +1,5 @@
 <?php
-	require_once(dirname(__DIR__).'/config.php');
-	//define("_SMARTY_",str_replace('\\','/',dirname(__FILE__)));
-	//__FILE__是PHP中的预定义常量，表示当前文件所在路径。str_replce函数把
-	// "\"换成"/"目的是为了兼容在linux操作系统下的目录
-	require_once(_COMMON_.'/smarty/libs/Smarty.class.php');
-	$smarty = new Smarty();
-	//$smarty->setTemplateDir(_SMARTY_.'/templates');
-	$smarty->setTemplateDir('../view');
-	$smarty->setCompileDir(_INDEX_.'/templates_c');
-	$smarty->right_delimiter='}>';
-	$smarty->left_delimiter='<{';
-	$smarty->assign('_PUBLIC_',_PUBLIC_);
-	$smarty->assign('_COMMON_',_COMMON_);
-	$smarty->assign('_APP_',_APP_);
-	$smarty->assign('_INDEX_',_INDEX_);
+	dfgdfhgfhjgfjgfj
+	
 	
 ?>
