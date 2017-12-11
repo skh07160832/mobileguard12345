af1734b4a48bd9cb2c680611acac1c04b7200c52
施凯焕 <31606844+skh07160832@users.noreply.github.com>
2017-12-11 14:14:43 +0800

    Update linkscript.php

--- common/link/linkscript.php
+++ common/link/linkscript.php
@@ -1,21 +1,3 @@
 <?php
-//require_once('../../config.php');
-$link_l="<link rel='stylesheet' type='text/css' href='";	
-$link_r="'/>";
-
-$script_l='<script type="text/javascript" src="';
-$script_r='"></script>';
-
-$rn="\r\n\t";
-
-$arr=[
- $link_l._PUBLIC_.'/static/assets/font-awesome-4.7.0/css/font-awesome.min.css'.$link_r,
- $link_l._PUBLIC_.'/static/assets/bootstrap-3.3.7/css/bootstrap.min.css'.$link_r,
- $script_l._PUBLIC_.'/static/assets/js/jquery-1.10.2.js'.$script_r,
- $script_l._PUBLIC_.'/static/assets/bootstrap-3.3.7/js/bootstrap.min.js'.$script_r,
- $script_l._PUBLIC_.'/static/assets/js/highcharts.js'.$script_r
-];
-
-$a=implode($rn,$arr)."\r\n";
-echo $a;
+erfetertret
 ?>
