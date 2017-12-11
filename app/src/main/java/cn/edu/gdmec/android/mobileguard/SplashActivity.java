33d683f26c3bb50b3f3424f8be068a1d93e0cf93
施凯焕 <31606844+skh07160832@users.noreply.github.com>
2017-12-11 15:37:10 +0800

    Add files via upload

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.mobileguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override//我是你大爷！！
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
